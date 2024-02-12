10f4b32bfd0a83bc48cd767f134fc42a08edd371
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2024-02-12 14:08:47 +0300

    toolbar&menu

---
+++ ArtBook_Toolbar&Menu/Java_codes/MainActivity.java
@@ -0,0 +1,47 @@
+package com.example.artbookjava;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+//import android.widget.Toolbar;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.toolbar_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId() == R.id.add_art){
+            Intent intent = new Intent(this,ArtActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+}
+
+
+
+
+
+
+
+
