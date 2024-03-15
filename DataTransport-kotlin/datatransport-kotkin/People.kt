7e07384cf5db2249939e667d4e445184e305c42f
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2024-03-15 12:52:33 +0300

    Update People.kt

--- DataTransport-kotlin/datatransport-kotkin/People.kt
+++ DataTransport-kotlin/datatransport-kotkin/People.kt
@@ -7,3 +7,12 @@ class People (var tcNo : Int , var name :String, var age : Int) :Serializable {
 
 
 }
+
+/*
+
+Serileştirme, bir objedeki verileri bir akışa (dosya, ağ bağlantısı vb.) yazarak veya bu akıştan okuyarak 
+aktarılabilir bir formata dönüştürme işlemidir. Serileştirme işlemi sayesinde nesnelerin durumu, verileri ve yapıları korunarak 
+başka bir yerde kullanılabilir veya saklanabilir hale gelir.
+Java'da serileştirme, bir nesnenin Serializable interface'ini uygulayarak gerçekleştirilir.
+
+*/
