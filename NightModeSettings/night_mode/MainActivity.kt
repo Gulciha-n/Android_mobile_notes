65989bc78880b4e1448db8f19ae01e81080eeb1a
Gülcihan <120305183+Gulciha-n@users.noreply.github.com>
2024-03-24 10:02:30 +0300

    Initial commit

---
+++ NightModeSettings/night_mode/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.gulcihan.night_mode
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatDelegate
+import com.gulcihan.night_mode.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private  lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.buttonNormal.setOnClickListener {
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_NO
+        }
+
+        binding.buttonNight.setOnClickListener {
+            delegate.localNightMode = AppCompatDelegate.MODE_NIGHT_YES
+        }
+    }
+}
